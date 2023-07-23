21ccb9bbe43685cff9677bff52f788b089fd1c14
Williem Berg Oliveira <wbog@discente.ifpe.edu.br>
2023-07-22 22:38:34 -0300

    mod

--- padalegria_front/script.js
+++ padalegria_front/script.js
@@ -1,48 +1,11 @@
-// Dados do sistema - Pães cadastrados e fornadas recentes
-const paoList = [];
-const recentFornadas = [];
+// Definição das informações sobre os pães
+const paoTypes = ["Francês", "Pão Bolacha", "Carteiro", "Doce", "Doce com Côco", "Pão Baiano"];
+const paoColors = ["#FF5733", "#FFC300", "#33FF57", "#33D6FF", "#336CFF", "#9233FF"];
 
-// Função para adicionar um novo pão ao sistema
-function addPao(type, description, time) {
-    paoList.push({ type, description, time });
-}
+// Definição das listas de pães e fornadas (usaremos arrays para simular o armazenamento temporário)
+let listaPaes = [];
+let listaFornadas = [];
 
-// Função para registrar uma nova fornada
-function registrarFornada(index) {
-    const pao = paoList[index];
-    const date = new Date();
-    recentFornadas.push({ type: pao.type, description: pao.description, time: pao.time, date });
-    showRecentFornadas();
-}
-
-// Função para mostrar as últimas fornadas na tela
-function showRecentFornadas() {
-    const breadInfo = document.getElementById("bread-info");
-    breadInfo.innerHTML = "";
-    recentFornadas.forEach((fornada) => {
-        const now = new Date();
-        const timeDiff = (fornada.date.getTime() + fornada.time * 60000) - now.getTime();
-        const timeRemaining = Math.ceil(timeDiff / 1000 / 60);
-        const timeDisplay = timeRemaining > 0 ? `Falta ${timeRemaining} minutos` : "Pronto!";
-        const info = document.createElement("p");
-        info.innerText = `${fornada.type} (${fornada.description}): ${fornada.date.toLocaleTimeString()} - ${timeDisplay}`;
-        breadInfo.appendChild(info);
-    });
-}
-
-// Função para criar os botões dos pães cadastrados
-function createBreadButtons() {
-    const breadButtons = document.getElementById("bread-buttons");
-    breadButtons.innerHTML = "";
-    paoList.forEach((pao, index) => {
-        const button = document.createElement("button");
-        button.innerText = pao.type;
-        button.classList.add("bread-button");
-        button.style.backgroundColor = `hsl(${index * 30}, 70%, 50%)`;
-        button.addEventListener("click", () => registrarFornada(index));
-        breadButtons.appendChild(button);
-    });
-}
 // Função para criar os botões de cadastro de fornada
 function createBakeButtons() {
     const breadButtons = document.getElementById("bread-buttons");
@@ -57,19 +20,36 @@ function createBakeButtons() {
     bindBakeButtons();
 }
 
+// Função para registrar a fornada
+function registrarFornada(index) {
+    const type = paoTypes[index];
+    const now = new Date();
+    const time = Math.floor(Math.random() * 120) + 30; // Tempo de preparo aleatório entre 30 e 150 minutos
+    const description = `Fornada ${listaFornadas.length + 1}`;
+    const fornada = { type, description, date: now, time };
+    listaFornadas.push(fornada);
+}
+
 // Função para mostrar as últimas fornadas na tela do cliente
 function showRecentFornadasCliente() {
     const breadInfo = document.getElementById("bread-info");
-    breadInfo.innerHTML = "";
-    recentFornadas.forEach((fornada) => {
-        const now = new Date();
-        const timeDiff = (fornada.date.getTime() + fornada.time * 60000) - now.getTime();
-        const timeRemaining = Math.ceil(timeDiff / 1000 / 60);
-        const timeDisplay = timeRemaining > 0 ? `Falta ${timeRemaining} minutos` : "Pronto!";
-        const info = document.createElement("p");
-        info.innerText = `${fornada.type} (${fornada.description}): ${fornada.date.toLocaleTimeString()} - ${timeDisplay}`;
-        breadInfo.appendChild(info);
-    });
+    if (breadInfo) {
+        breadInfo.innerHTML = "";
+        listaPaes.forEach((pao) => {
+            const fornadasPao = listaFornadas.filter((fornada) => fornada.type === pao);
+            if (fornadasPao.length > 0) {
+                fornadasPao.forEach((fornada) => {
+                    const now = new Date();
+                    const timeDiff = (fornada.date.getTime() + fornada.time * 60000) - now.getTime();
+                    const timeRemaining = Math.ceil(timeDiff / 1000 / 60);
+                    const timeDisplay = timeRemaining > 0 ? `Falta ${timeRemaining} minutos` : "Pronto!";
+                    const info = document.createElement("p");
+                    info.innerText = `${fornada.type} (${fornada.description}): ${fornada.date.toLocaleTimeString()} - ${timeDisplay}`;
+                    breadInfo.appendChild(info);
+                });
+            }
+        });
+    }
 }
 
 // Evento de cadastro de fornada pelo cliente
